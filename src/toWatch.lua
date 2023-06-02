955dca888b095b2aa2631b68adad446fa6092896
HumbleSolipsist <HumbleSolipsist@cs.sfu.ca>
2023-06-01 23:22:40 -0700

    Update blurbs

--- src/toWatch.lua
+++ src/toWatch.lua
@@ -366,7 +366,7 @@ return {
   {tier = "C", year = 2022, title = "Dragon Lizard Lord Super Monsters",
    poster = "https://img1.wsimg.com/isteam/ip/c3627ab8-fa28-42d0-bbaa-4b2a11ae3850/dlsmaller.jpg/:/cr=t:0%25,l:0%25,w:100%25,h:100%25/rs=w:1300,h:800",
    blurb = "The incomprehensible and repetitive stream of disjoint audio and video "
-        .. "acts like a hypnotist's pendulum, captivating the audience. Still, it's "
+        .. "has a hypnotic effect, captivating the audience. Still, it's "
         .. "not exactly a virtuous film."},
   {tier = nil, year = 2022, title = "Hammer of Draco Wrath of Dragons"},
   {tier = nil, year = nil,  title = "Dragon Lizard Lord vs Gorilla Monkey King"},
@@ -484,10 +484,9 @@ return {
   {tier = nil, year = 1958, title = "War of the Colossal Beast"},
   {tier = "C", year = 2007, title = "Big Man Japan",
    poster = "https://upload.wikimedia.org/wikipedia/en/thumb/6/65/BigManJapan.jpg/220px-BigManJapan.jpg",
-   blurb = "A mockumentary about a man who's struggling to preserve his family's "
-        .. "business of growing giant to fight monsters. The jokes rarely landed "
-        .. "for me, but I did enjoy the melancholic and frank look into the life "
-        .. "of a professional hero."},
+   blurb = "A man struggles to preserve his family business of growing giant to "
+        .. "fight monsters. The jokes rarely land, but I enjoy the melancholic and "
+        .. "frank look into the life of a professional hero."},
 
   -- Giantesses
   {tier = "C", year = 1958, title = "Attack of the 50 Foot Woman",
